aaafbe2d59bbe50388b146850735a1ee3e5227ae
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2017-11-14 19:24:06 +0100

    perimeter of triangle

--- Chapter_03/Exercise_19.java
+++ Chapter_03/Exercise_19.java
@@ -8,8 +8,23 @@ if the sum of every pair of two edges is greater than the remaining edge.
 public class Exercise_19 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double perimeter = 0;
 
         // prompt for triangle sides
+        System.out.println("Put length of first side: ");
+        double a = input.nextDouble();
+        System.out.println("Put length of first side: ");
+        double b = input.nextDouble();
+        System.out.println("Put length of first side: ");
+        double c = input.nextDouble();
 
+        // check if every pair is greater then other one edge
+        if (a + b > c && b + c > a && a + c > b){
+            System.out.println("Correct edges.");
+            perimeter = a + b + c;
+            System.out.println("Perimeter: " + perimeter);
+        } else {
+            System.out.println("Incorrect edges.");
+        }
     }
 }
