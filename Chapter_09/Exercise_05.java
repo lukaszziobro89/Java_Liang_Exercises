8721ef1d55ec03af8bad7d9c9bc009f06f1225eb
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2017-11-18 13:35:17 +0100

    gregorian calendar with dates

--- Chapter_09/Exercise_05.java
+++ Chapter_09/Exercise_05.java
@@ -1,4 +1,13 @@
 package Chapter_09;
+/*
+(Use the GregorianCalendar class) Java API has the GregorianCalendar class in the java.util package, which you can use
+to obtain the year, month, and day of a date. The no-arg constructor constructs an instance for the current date, and
+the methods get(GregorianCalendar.YEAR), get(GregorianCalendar.MONTH), and get(GregorianCalendar.DAY_OF_MONTH) return
+the year, month, and day. Write a program to perform two tasks:
+    ■ Display the current year, month, and day.
+    ■ The GregorianCalendar class has the setTimeInMillis(long), which
+can be used to set a specified elapsed time since January 1, 1970. Set the value to 1234567898765L and display the year, month, and day.
+ */
 import java.util.GregorianCalendar;
 public class Exercise_05 {
     public static void main(String[] args) {
