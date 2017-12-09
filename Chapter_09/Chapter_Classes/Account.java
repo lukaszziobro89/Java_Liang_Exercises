e9f36d09bff8a487a6e262c553079118ed7649fa
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2017-12-09 22:45:48 +0100

    Account class - modified for Transaction class

--- Chapter_09/Chapter_Classes/Account.java
+++ Chapter_09/Chapter_Classes/Account.java
@@ -1,12 +1,17 @@
 package Chapter_09.Chapter_Classes;
+import Chapter_11.Chapter_Classes.Transaction;
+
+import java.util.ArrayList;
 import java.util.Date;
 public class Account {
 
     // fields (attributes)
     private int id;
+    private String name;
     private double balance;
     private double annualInterestRate;
     private Date dateCreated;
+    private ArrayList<Transaction> accTransactions;
 
     // Accessor and mutator methods for id, balance, and annualInterestRate.
     public int getId() {
@@ -38,6 +43,17 @@ public class Account {
         return dateCreated.toString();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Transaction> getAccTransactions() {
+        return new ArrayList<>(accTransactions);
+    }
 
     // A no-arg constructor that creates a default account.
     public Account(){
@@ -53,12 +69,25 @@ public class Account {
         this.balance = balance;
     }
 
-    // A constructor that creates an account with the specified id and initial balance.
     public Account(int id, double balance, double annualInterestRate){
         this.id = id;
         this.balance = balance;
         this.annualInterestRate = annualInterestRate;
+    }
+
+    public Account(int id, String name, double balance, double annualInterestRate) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
         this.dateCreated = new Date();
+        this.accTransactions = new ArrayList<>();
+    }
+
+    // constructor that constructs an account with the specified name, id, and balance.
+    public Account(int id, String name, double balance){
+        this(id, balance);
+        this.name = name;
     }
 
     /** A method named getMonthlyInterestRate() that returns the monthly interest rate. */
@@ -73,11 +102,13 @@ public class Account {
 
     /** A method named withdraw that withdraws a specified amount from the account. */
     public void withdraw(double amount){
+        accTransactions.add(new Transaction('W', amount, balance, "withdraw"));
         balance -= amount;
     }
 
     /** A method named deposit that deposits a specified amount to the account. */
     public void deposit(double deposit){
+        accTransactions.add(new Transaction('D', deposit, balance, "deposit"));
         balance += deposit;
     }
 
@@ -91,5 +122,11 @@ public class Account {
                 .append("\tAccount create date: ").append(this.getDateCreated()).append("\n");
 
         return accountInformation;
+    }
+
+    /** Overrided toString method */
+    @Override
+    public String toString() {
+        return "Account name: " + name + "\n" + "Interest rate: " + annualInterestRate + "\n" + accTransactions;
     }
 }
