12733421e7bd8466eeea6878d3906763113560f0
Lukasz Ziobro <lukasz.ziobro.89@gmail.com>
2017-12-13 13:25:29 +0100

    ComparableCircle example

---
+++ Chapter_13/Exercise_06.java
@@ -0,0 +1,18 @@
+package Chapter_13;
+import Chapter_11.Chapter_Classes.GeometricObject;
+import Chapter_13.Chapter_Classes.AbstractGeometricObject;
+import Chapter_13.Chapter_Classes.ComparableCircle;
+/*
+(The ComparableCircle class) Define a class named ComparableCircle that extends Circle and implements Comparable.
+Implement the compareTo method to compare the circles on the basis of area.
+Write a test class to find the larger of two instances of ComparableCircle objects.
+ */
+public class Exercise_06 {
+    public static void main(String[] args) {
+        // create 2 instances of ComparableCircle object
+        ComparableCircle c1 = new ComparableCircle(0,0,3);
+        ComparableCircle c2 = new ComparableCircle(0,0,5);
+        // compare which is greater
+        System.out.println(c1.compareTo(c2));
+    }
+}
